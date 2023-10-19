2ae318410eaab93785dc615db9e6fa9226ed6dc8
tuy112 <master_catch@naver.com>
2023-10-19 11:15:01 +0900

    fix: 뱀 움직임 변경 +  고기 추가

--- index.js
+++ index.js
@@ -1,27 +1,35 @@
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
+// 뱀
 let positionX = 0,
   positionY = 0;
 const gridSize = 20,
   tileCount = 20;
+let velocityX = 0,
+  velocityY = 0;
+
+// 고기
+let meatX = 15,
+  meatY = 15;
+
 function keyPush(evt) {
   switch (evt.keyCode) {
     case 37:
-      positionX += -1;
-      positionY += 0;
+      velocityX += -1;
+      velocityY += 0;
       break;
     case 38:
-      positionX += 0;
-      positionY += -1;
+      velocityX += 0;
+      velocityY += -1;
       break;
     case 39:
-      positionX += 1;
-      positionY += 0;
+      velocityX += 1;
+      velocityY += 0;
       break;
     case 40:
-      positionX += 0;
-      positionY += 1;
+      velocityX += 0;
+      velocityY += 1;
       break;
   }
 }
@@ -40,6 +48,13 @@ function game() {
     gridSize - 2
   );
 
+  // 고기
+  ctx.fillStyle = "brown";
+  ctx.fillRect(meatX * gridSize, meatY * gridSize, gridSize - 2, gridSize - 2);
+
+  positionX += velocityX;
+  positionY += velocityY;
+
   if (positionX < 0) {
     positionX = tileCount - 1;
   }
@@ -56,6 +71,5 @@ function game() {
 
 window.onload = () => {
   document.addEventListener("keydown", keyPush);
-
   setInterval(game, 1000 / 15);
 };

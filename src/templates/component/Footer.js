b48e47f3e4152a3b23ee93c88e762b3ff30a2f5b
Kim Jae Hyeok <master_catch@naver.com>
2025-01-14 19:13:00 +0900

    [공통] Footer 컴포넌트 수정

--- src/templates/component/Footer.js
+++ src/templates/component/Footer.js
@@ -8,29 +8,31 @@ import youtube from "../../static/images/YoutubeLogo.png";
 function Footer() {
     return (
         <footer id="footer">
-            <h3>CONTACT ME</h3>
-            <ul className="links">
-                <li>
-                    <Link to="https://github.com/tuy112">
-                        <img src={github} alt="깃허브" />
-                        <em>GITHUB</em>
-                    </Link>
-                </li>
-                <li>
-                    <Link to="https://jh-healing-place.tistory.com/">
-                        <img src={blog} alt="티스토리" />
-                        <em>BLOG</em>
-                    </Link>
-                </li>
-                <li>
-                    <Link to="https://www.youtube.com/channel/UCIUZejYbHXZOHhwUokcRBUQ">
-                        <img src={youtube} alt="유튜브" />
-                        <em>YOUTUBE</em>
-                    </Link>
-                </li>
-            </ul>
-            <div className="always">
-                <p>Future Korean NO.1 Developer</p>
+            <div className="inner">
+                <h3>CONTACT ME</h3>
+                <ul className="links">
+                    <li>
+                        <Link to="https://github.com/tuy112">
+                            <img src={github} alt="깃허브" />
+                            <em>GITHUB</em>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="https://jh-healing-place.tistory.com/">
+                            <img src={blog} alt="티스토리" />
+                            <em>BLOG</em>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="https://www.youtube.com/channel/UCIUZejYbHXZOHhwUokcRBUQ">
+                            <img src={youtube} alt="유튜브" />
+                            <em>YOUTUBE</em>
+                        </Link>
+                    </li>
+                </ul>
+                <div className="always">
+                    <p>Do your Best, Then Good Result follow you :&#41;</p>
+                </div>
             </div>
         </footer>
     );

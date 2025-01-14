28b4b1d19318794cc497bb8d8979d338f3111019
Kim Jae Hyeok <master_catch@naver.com>
2025-01-14 19:13:28 +0900

    [회원가입] 페이지 신규 추가

---
+++ src/templates/pages/Signup.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+const Signup = () => {
+
+    return(
+        <div id="wrap">
+            {/* 임시 */}
+            <h5>testUser : abc1234 / pw : 1234</h5>
+        </div>
+    );
+}
+
+export default Signup;

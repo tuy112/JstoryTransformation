d11c7d04bae8f20aa488d91b923b1011545b0ee1
Kim Jae Hyeok <master_catch@naver.com>
2025-02-05 22:08:41 +0900

    App.js 최적화 작업

--- src/App.js
+++ src/App.js
@@ -3,47 +3,40 @@ import { Routes, Route } from 'react-router-dom';
 import './static/css/common/reset.css';
 import './static/css/common/gate.css';
 
-// common
+// Components
 import Gate from './templates/pages/common/Gate';
 import Home from './templates/pages/common/Home';
-
-// main
 import Profile from './templates/pages/Profile';
 import Project from './templates/pages/Project';
 import History from './templates/pages/History';
-
-// login
 import Login from './templates/pages/Login';
 import Signup from './templates/pages/Signup';
-
-// Projects
 import Todo from './templates/projects/todo/Todo';
-// import Mountain from './templates/projects/mountain/Mountain';
+import Mountain from './templates/projects/mountain/Mountain';
 import Three from './templates/projects/three/Three';
 
-
-
-function App() {
-
+const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Gate/>}></Route>
-      <Route path="/home" element={<Home/>}></Route>
-
-      {/* pages */}
-      <Route path="/profile" element={<Profile/>}></Route>
-      <Route path="/project" element={<Project/>}></Route>
-      <Route path="/history" element={<History/>}></Route>
-
-      {/* signUp + login */}
-      <Route path="/login" element={<Login/>}></Route>
-      <Route path="/signup" element={<Signup/>}></Route>
-
-      {/* project */}
-      <Route path="/todo" element={<Todo/>}></Route>
-      <Route path="/three" element={<Three/>}></Route>
+      {/* Main Routes */}
+      <Route path="/" element={<Gate />} />
+      <Route path="/home" element={<Home />} />
+      
+      {/* Page Routes */}
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/project" element={<Project />} />
+      <Route path="/history" element={<History />} />
+      
+      {/* Auth Routes */}
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      
+      {/* Project Routes */}
+      <Route path="/todo" element={<Todo />} />
+      <Route path="/three" element={<Three />} />
+      <Route path="/mountain" element={<Mountain />} />
     </Routes>
   );
-}
+};
 
-export default App; 
+export default App;

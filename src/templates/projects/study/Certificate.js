6b439f2380e31544c34a2376b5fbac00ef15d499
Kim Jae Hyeok <master_catch@naver.com>
2025-03-28 00:01:32 +0900

    [Sub] 나만의 공부방 뼈대 만들기

---
+++ src/templates/projects/study/Certificate.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import Header from "../../component/Header";
+import Footer from "../../component/Footer";
+
+import '../../../static/css/common/reset.css';
+import '../../../static/css/common/common.css';
+import './style/study.css';
+
+const Certificate = () => {
+    
+
+    return(
+        <div id="wrap">
+            <Header/>
+
+
+            <Footer/>
+        </div>
+    );
+}
+
+export default Certificate;
